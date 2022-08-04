e9d63bd5e671a30665b041a0767cbce87de61af0
Jeff Free <jefffreelance3@gmail.com>
2022-08-04 01:10:39 -0300

    redux extension added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Col } from "antd";
-import { connect } from "react-redux";
-import { setPokemons as setPokemonsActions } from "./actions";
+import { useSelector, useDispatch } from "react-redux";
+import { setPokemons } from "./actions";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import logo from "./static/logo.svg";
@@ -8,12 +8,15 @@ import "./App.css";
 import { useEffect } from "react";
 import { getPokemon } from "./api";
 
-function App({ pokemons, setPokemons }) {
+function App() {
+
+	const pokemons = useSelector(state => state.pokemons);
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		const fetchPokemons = async () => {
 			const pokemonsRes = await getPokemon();
-			setPokemons(pokemonsRes);
+			dispatch(setPokemons(pokemonsRes));
 		};
 
 		fetchPokemons();
@@ -32,12 +35,4 @@ function App({ pokemons, setPokemons }) {
 	);
 }
 
-const mapStateToProps = (state) => ({
-	pokemons: state.pokemons,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-	setPokemons: (value) => dispatch(setPokemonsActions(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
